c8d83f7f7c48b391a490aa0af4b51d83e9e77f4c
adil.moumni <49393758+adilmoumni@users.noreply.github.com>
2025-02-25 15:54:43 +0000

    add to main

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ function run(): void {
   try {
     // Get the input provided by the user (or the default value)
     const code: string = core.getInput('code-to-eval');
-    console.log(`Hello, ${name}!`);
+    console.log(`Hello, ${code}!`);
   } catch (error: any) {
     core.setFailed(error.message);
   }
