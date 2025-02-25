0aa5733de99884f2f2f94581775476bbb520bfeb
adil.moumni <49393758+adilmoumni@users.noreply.github.com>
2025-02-25 15:46:48 +0000

    Initial commit of TypeScript GitHub Action

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as core from '@actions/core';
+
+function run(): void {
+  try {
+    // Get the input provided by the user (or the default value)
+    const code: string = core.getInput('code-to-eval');
+    console.log(`Hello, ${name}!`);
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
